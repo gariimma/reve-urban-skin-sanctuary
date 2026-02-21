d78c14f8063d95b7d0beebd81994d7673448ed35
Garima <73426804+gariimma@users.noreply.github.com>
2026-02-21 15:58:31 +0530

    Update tailwind.config.ts
    Added marquee animation for announcement bar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,12 +84,17 @@ export default {
           from: { opacity: "0" },
           to: { opacity: "1" },
         },
+        "marquee": {
+        from: { transform: "translateX(0)" },
+        to: { transform: "translateX(-50%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "fade-in-up": "fade-in-up 0.8s ease-out forwards",
         "fade-in": "fade-in 1s ease-out forwards",
+        "marquee": "marquee 25s linear infinite",
       },
     },
   },
